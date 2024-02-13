7735f1c6c0001f6cdfd7b1532ebae923ad2cd7d1
AliAsim99 <ali.99asim@gmail.com>
2024-02-14 03:45:10 +0500

    w

--- index.js
+++ index.js
@@ -10,33 +10,10 @@ const Person=require('./models/person')
 const morgan=require('morgan')
 app.use(morgan('tiny'))
 
-let persons=[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
-app.get("/info",(req,res)=>{
-    res.send(`<p>Phonebook has info for ${persons.length}</p>`)
-})
+
 app.get("/api/persons",(req,res)=>{
     Person.find({})
     .then(persons=>res.json(persons))
@@ -50,6 +27,7 @@ app.get("/api/persons/:id",(req,res)=>{
             res.status(404).end()
         }
     })
+    .catch(error=>next(error))
    
 })
 const generateId=()=>{
@@ -76,14 +54,21 @@ app.post("/api/persons",(req,res)=>{
    
 })
 app.delete("/api/persons/:id",(req,res)=>{
-    const id=Number(req.params.id)
-    console.log(id)
-    persons=persons.filter(p=>p.id!=id)
+    Person.findByIdAndDelete(req.params.id)
+    .then(res=>{
+        res.status(204).end()
+    })
+    .catch(error=>next(error))
 
-    res.status(204).end()
 
 })
-
+const errorHandler=(error,res,req,next)=>{
+    if(error.name=='CastError'){
+        return res.status(400).send({error:'malformated id'})
+    }
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT=process.env.PORT||3001;
 app.listen(PORT,()=>console.log(`running on port ${PORT}`))
